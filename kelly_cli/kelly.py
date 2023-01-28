547dd1453fe0916dfcc035f2391d3c0558eda341
peaky76 <robertjamespeacock@gmail.com>
2023-01-28 00:23:23 +0000

    Create kelly cli with decimal/prob/percent and bank options
    Includes testing and gitignore

---
+++ kelly_cli/kelly.py
@@ -0,0 +1,22 @@
+import click
+from pybet import Odds
+from pybet.staking import kelly as pykelly
+
+
+@click.command()
+@click.option('-d', '--decimal', 'odds_type', flag_value='decimal', default=True, help='interpret inputs as decimal odds')
+@click.option('-p', '--prob', 'odds_type', flag_value='prob', help='interpret inputs as probabilities')
+@click.option('-c', '--percent', 'odds_type', flag_value='percent', help='interpret inputs as percentages')
+@click.argument('true_odds', type=click.FLOAT)
+@click.argument('market_odds', type=click.FLOAT)
+@click.option('-b', '--bank', default=100, help='the size of betting bank')
+def kelly(odds_type, true_odds, market_odds, bank):
+
+    constructor = {
+        'decimal': Odds,
+        'prob': Odds.probability,
+        'percent': Odds.percentage
+    }[odds_type]
+
+    stake = pykelly(constructor(true_odds), constructor(market_odds), bank)
+    click.echo(f'£{round(stake, 2)}')
---
+++ tests/test_kelly.py
@@ -0,0 +1,54 @@
+from click.testing import CliRunner
+from kelly_cli.kelly import kelly
+
+
+def test_decimal():
+    runner = CliRunner()
+    result = runner.invoke(kelly, ['4', '5'])
+    assert result.exit_code == 0
+    assert "£6.25" in result.output
+
+
+def test_probability_abbr():
+    runner = CliRunner()
+    result = runner.invoke(kelly, ['-p', '0.25', '0.2'])
+    assert result.exit_code == 0
+    assert "£6.25" in result.output
+
+
+def test_probability_full():
+    runner = CliRunner()
+    result = runner.invoke(kelly, ['--prob', '0.25', '0.2'])
+    assert result.exit_code == 0
+    assert "£6.25" in result.output
+
+
+def test_percentage_abbr():
+    runner = CliRunner()
+    result = runner.invoke(kelly, ['-c', '25', '20'])
+    assert result.exit_code == 0
+    assert "£6.25" in result.output
+
+
+def test_percentage_full():
+    runner = CliRunner()
+    result = runner.invoke(kelly, ['--percent', '25', '20'])
+    assert result.exit_code == 0
+    assert "£6.25" in result.output
+
+
+def test_bank():
+    runner = CliRunner()
+    result = runner.invoke(kelly, ['4', '5', '-b', '200'])
+    assert result.exit_code == 0
+    assert "£12.50" in result.output
+
+
+def runner():
+    test_decimal()
+    test_probability_abbr()
+    test_probability_full()
+    test_percentage_abbr()
+    test_percentage_full()
+    test_bank()
+    print('6 tests passed successfully')
